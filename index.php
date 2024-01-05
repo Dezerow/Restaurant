7f327af3858e780f33b05321d1dc8662d48a5d81
Dezerow <darth3160@gmail.com>
2024-01-05 16:22:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css" >
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Strona główna</title>
+</head>
+<body>
+    <div class="container-fluid px-0">
+        <section name="restaurantName" id="restaurantName">
+         <h1>Tanio i jakościowo</h1>
+        </section>
+        <section name="mainDish" id="mainDish">
+            <div class="row h-100">
+                <div class="col-lg-5 col-md-12" id="mainDishLeft">
+                </div>
+                <div class="col-lg-7 col-md-12 mt-1" id="mainDishRight">
+                    <h3>Dania główne</h3>
+                    <div>
+                        <h5>Tradycyjny kotlet schabowy</h5>
+                        <p>Ziemniaki z wody, kapusta zasmażana</p>
+                        <p>33zł</p>
+                    </div>
+                    <div>
+                        <h5>XXL Kotlet schabowy</h5>
+                        <p>Ziemniaki z wody, kapusta zasmażana</p>
+                        <p>45zł</p>
+                    </div>                                               
+                </div>    
+            </div>
+        </section>
+
+        <section name="pasta" id="pasta">
+        <div class="row h-100">
+                <div class="col-lg-5 col-md-12 mt-1" id="pastaLeft">
+                    <h3>Makarony</h3>
+                    <div>
+                        <h5>Tradycyjny kotlet schabowy</h5>
+                        <p>Ziemniaki z wody, kapusta zasmażana</p>
+                        <p>33zł</p>
+                    </div>
+                    <div>
+                        <h5>XXL Kotlet schabowy</h5>
+                        <p>Ziemniaki z wody, kapusta zasmażana</p>
+                        <p>45zł</p>
+                    </div>                                              
+                </div>
+                <div class="col-lg-7 col-md-12" id="pastaRight">                    
+                </div>    
+            </div>
+        </section>
+
+        <section name="fish" id="fish">
+            <div class="row h-100">
+                <div class="col-lg-5 col-md-12" id="fishLeft">
+                </div>
+                <div class="col-lg-7 col-md-12 mt-1" id="fishRight">
+                    <h3>Ryby</h3>
+                    <div>
+                        <h5>Tradycyjny kotlet schabowy</h5>
+                        <p>Ziemniaki z wody, kapusta zasmażana</p>
+                        <p>33zł</p>
+                    </div>
+                    <div>
+                        <h5>XXL Kotlet schabowy</h5>
+                        <p>Ziemniaki z wody, kapusta zasmażana</p>
+                        <p>45zł</p>
+                    </div>                                               
+                </div>    
+            </div>
+        </section>
+
+        <section name="soups" id="soups">
+            <div class="row h-100">
+            <div class="col-lg-5 col-md-12 mt-1" id="soupsLeft">
+                    <h3>Zupy</h3>
+                    <div>
+                        <h5>Tradycyjny kotlet schabowy</h5>
+                        <p>Ziemniaki z wody, kapusta zasmażana</p>
+                        <p>33zł</p>
+                    </div>
+                    <div>
+                        <h5>XXL Kotlet schabowy</h5>
+                        <p>Ziemniaki z wody, kapusta zasmażana</p>
+                        <p>45zł</p>
+                    </div>                                              
+                </div>
+                <div class="col-lg-7 col-md-12" id="soupsRight">                    
+                </div>    
+            </div>
+        </section>
+    
+    </div>
+    <script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
